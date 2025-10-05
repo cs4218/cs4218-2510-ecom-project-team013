451005888679f7ddb6cc73edf257dadf2b37622b
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-10-05 19:01:55 +0800

    feat: Improve `useCart` typing and safety

--- client/src/context/cart.tsx
+++ client/src/context/cart.tsx
@@ -1,8 +1,17 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import type { Product } from "../../../models/productModel";
 
-const CartContext = createContext();
-const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+export type CartItem = Product;
+
+type CartContextType = [
+  CartItem[],
+  React.Dispatch<React.SetStateAction<CartItem[]>>,
+];
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+const CartProvider = ({ children }: { children: React.ReactNode }) => {
+  const [cart, setCart] = useState<CartItem[]>([]);
 
   useEffect(() => {
     let existingCartItem = localStorage.getItem("cart");
@@ -16,7 +25,13 @@ const CartProvider = ({ children }) => {
   );
 };
 
-// custom hook
-const useCart = () => useContext(CartContext);
+// Custom hook
+const useCart = (): CartContextType => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+};
 
 export { CartProvider, useCart };
