d6758c2e877bf78ca12933ad17ffcdbb2b4871be
Austintjh19 <austintjh19@gmail.com>
2025-10-20 01:36:16 +0800

    fix: seed db issue

--- config/db.ts
+++ config/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URL!);
     console.log(
       `Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white
     );
