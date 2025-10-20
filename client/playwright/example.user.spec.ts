c1981191f2c194532bd2ff50549ff67b6149b2b4
Austintjh19 <austintjh19@gmail.com>
2025-10-20 22:14:06 +0800

    feat: add separate authentication setups for admin and user

---
+++ client/playwright/example.user.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("All auth pages Renders", () => {
+  test("Test Pages Rendering", async ({ page }) => {
+    await page.goto("/");
+    await expect(
+      page.getByRole("button", { name: "Charlie Lee" })
+    ).toBeVisible();
+
+    await expect(
+      page.getByRole("heading", { name: "All Products" })
+    ).toBeVisible();
+
+    await page.getByRole("link", { name: "Categories" }).click();
+    await page.getByRole("link", { name: "All Categories" }).click();
+    await expect(
+      page.getByText("ElectronicsBooksFashion", { exact: true })
+    ).toBeVisible();
+  });
+});
--- client/playwright/setup/admin.setup.ts
+++ client/playwright/setup/admin.setup.ts
@@ -1,7 +1,7 @@
 import { expect, test as setup } from "@playwright/test";
 import path from "path";
 
-const authFile = path.join(__dirname, "../.auth/user.json");
+const authFile = path.join(__dirname, "../.auth/admin.json");
 
 setup("authenticate", async ({ page }) => {
   // Perform authentication steps. Replace these actions with your own.
